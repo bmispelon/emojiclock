1e4a1ea4dbc09b1839d8b0bb296a988953d46d2a
Baptiste Mispelon <bmispelon@gmail.com>
2018-11-23 16:42:17 +0100

    Added custom 404 and 500 error pages

--- emojiclock/urls.py
+++ emojiclock/urls.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError, JsonResponse
 from django.urls import path
 from django.utils import timezone
 
 from .utils import time_to_emoji
 
 
-PAGE_TEMPLATE = """
+FULL_PAGE_EMOJI_TEMPLATE = """
 <!DOCTYPE html>
 <html>
   <head>
@@ -17,12 +17,18 @@ PAGE_TEMPLATE = """
         line-height: 100vh;
       }}
     </style>
-    <title>{clock}</title>
+    <title>{emoji}</title>
   </head>
-  <body>{clock}</body>
+  <body>{emoji}</body>
   <!-- Questions, issues? https://github.com/bmispelon/emojiclock -->
 </html>
 """
+
+
+
+def _emojiresponse(emoji, response_class=HttpResponse):
+    body = FULL_PAGE_EMOJI_TEMPLATE.format(emoji=emoji)
+    return response_class(body)
 
 
 def emojiclock(request):
@@ -34,11 +40,22 @@ def emojiclock(request):
     elif 'html' not in request.META['HTTP_ACCEPT']:
         response = HttpResponse(emoji, content_type='text/plain; charset=utf-8')
     else:
-        response = HttpResponse(PAGE_TEMPLATE.format(clock=emoji))
+        response = _emojiresponse(emoji=emoji)
 
     return response
 
 
+def handler500(request, *args, **kwargs):  # Not sure what arguments it should accept
+    return _emojiresponse('\N{POLICE CARS REVOLVING LIGHT}', response_class=HttpResponseServerError)
+
+
+def handler404(request, *args, **kwargs):
+    return _emojiresponse('\N{GHOST}', response_class=HttpResponseNotFound)
+
+
 urlpatterns = [
     path('', emojiclock, name='home'),
+
+    path('_500/', handler500),
+    path('_404/', handler404),
 ]
